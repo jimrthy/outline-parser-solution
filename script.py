6a9a95439482e3226b26ca396e0c77205d2f883a
James Gatannah <james.gatannah@gmail.com>
2018-10-04 15:30:05 -0500

    Fixed bulleted nesting style

--- script.py
+++ script.py
@@ -9,14 +9,18 @@ import re
 import sys
 
 BULLET_TAG = '.'
+# The rule for which bullet to use seems to depend on whether there's
+# increased nesting below it.
+FLAT_BULLET = '-'
+NEST_BULLET = '+'
 ORDERED_TAG = '*'
 
 TAG_EXTRACTOR = re.compile(r'\s')
 
 
-class Outliner:
+class TreeBuilder:
     """
-    Track the state of the outline and transform each line
+    Build up the outline in memory
     """
     def __init__(self):
         dispatcher = {
@@ -24,6 +28,7 @@ class Outliner:
             ORDERED_TAG: self.numbered_point
         }
 
+        # This is awkward, but the default dict balances that
         def continuation():
             return self.continuation
 
@@ -36,6 +41,41 @@ class Outliner:
         # Note that continuation lines get an extra 2 spaces
         self.unordered_indentation = 0
 
+        # For large data sets, this gets more complicated because it
+        # would really need to be stored on disk.
+        # That isn't a concern yet, but it could very well turn into
+        # one quickly.
+        self.parse_tree = []
+
+    def cope_with_bullet_nesting(self, tag: str) -> str:
+        """
+        Updates indentation and possibly the parent bullet style
+
+        Args:
+            tag: Tells us the indentation level for the current line
+
+        Returns: Meaningless. Called for side-effects.
+        """
+        # For large data sets, there's a good chance that this
+        # would make more sense as a second pass parser.
+        next_ul_indent = len(tag) + 1
+        if next_ul_indent > self.unordered_indentation:
+            # Search lines back from the most recent.
+            # Have to skip over continuations
+            previous = len(self.parse_tree) - 1
+            # This is almost obnoxious enough to convince me to use
+            # a data class instead of a plain pair.
+            while self.parse_tree[previous][0][-1] == ' ':
+                previous -= 1
+                if previous < 0:
+                    break
+            if previous >= 0 and \
+               self.parse_tree[previous][0][-1] == FLAT_BULLET:
+                replacement = (f'{self.parse_tree[previous][0][:-1]}'
+                               f'{NEST_BULLET}')
+                self.parse_tree[previous][0] = replacement
+        self.unordered_indentation = next_ul_indent
+
     def bullet_point(self, tag: str, body: str) -> str:
         """
         Like an ul in html
@@ -45,12 +85,9 @@ class Outliner:
             body: Line to process
         Returns: processed output
         """
-        self.unordered_indentation = len(tag) + 1
+        self.cope_with_bullet_nesting(tag)
         indentation = ' ' * self.unordered_indentation
-        # This is supposed to alternate between + and -, but I don't
-        # see any rhyme or reason to it yet.
-        bullet = '+'
-        return f'{indentation}{bullet} {body}'
+        return [f'{indentation}{FLAT_BULLET}', body]
 
     def continuation(self, first_word: str, remainder: str) -> str:
         """
@@ -61,10 +98,8 @@ class Outliner:
             remainder: Rest of line
         Returns: processed output
         """
-        #print(f'==Continuing: "{first_word}" and "{remainder}"')
-        indentation = ' ' * (self.unordered_indentation + 2)
-        result = f'{indentation}{first_word} {remainder}'
-        return result
+        indentation = ' ' * (self.unordered_indentation + 1)
+        return (indentation, f'{first_word} {remainder}')
 
     def increment_numbering(self, tag: str) -> str:
         """
@@ -113,35 +148,49 @@ class Outliner:
         """
         prefix = self.increment_numbering(tag)
 
-        return f'{prefix} {body}'
+        return (prefix, body)
 
-    def parse(self, line):
+    def parse(self, line: str):
         """
-        Handle the next line of input
+        Add another line of input to the parsed tree
+
+        Returns: Meaningless. Called for side-effects
         """
-        tag, body = re.split(TAG_EXTRACTOR, line, 1)
-        if tag:
-            symbol = tag[0]
-            if symbol in {BULLET_TAG, ORDERED_TAG}:
-                for character in tag[1:]:
-                    if character != tag[0]:
-                        # This is undefined, but seems safer than ignoring
-                        # the problem
-                        raise ValueError(tag)
-            processor = self.dispatcher[symbol]
-            result = processor(tag, body)
-            # FIXME: debug only
-            #print(f'Processing:\n"{line}"\nwith: {processor} Result:\n"{result}"')
-            return result
+        line = line.strip()
+        if line:
+            try:
+                tag, body = re.split(TAG_EXTRACTOR, line, 1)
+            except TypeError as ex:
+                print(f'{ex}\nTrying to split {line}')
+            if tag:
+                symbol = tag[0]
+                if symbol in {BULLET_TAG, ORDERED_TAG}:
+                    for character in tag[1:]:
+                        if character != symbol:
+                            # This is undefined, but seems safer than ignoring
+                            # the problem
+                            raise ValueError(tag)
+                processor = self.dispatcher[symbol]
+                result = processor(tag, body)
+                self.parse_tree.append(result)
+
+    def __str__(self) -> str:
+        """
+        Converts a ParseTree into a string
+        """
+        result = ''
+        for prefix, body in self.parse_tree:
+            result += f'{prefix} {body}\n'
+        return result
 
 
 def main(argv):
-    outliner = Outliner()
+    parser = TreeBuilder()
 
     for line in sys.stdin:
-        output = outliner.parse(line)
-        if output:
-            print(output, end='')
+        parser.parse(line)
+
+    print(str(parser))
 
 
 if __name__ == '__main__':
---
+++ test_script.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env pytest
+
+import script
+
+
+def test_bullet_nesting():
+    input = """* This is an outline
+
+. It's not a very good outline
+
+.. I've seen better
+
+.. I've seen worse
+
+... I saw a really bad one back in 2008
+    """
+    parser = script.TreeBuilder()
+    #breakpoint()
+    for line in input.split('\n'):
+        parser.parse(line)
+
+    actual = script.outline(parser)
+    expected = """1 This is an outline
+  + It's not a very good outline
+   - I've seen better
+   + I've seen worse
+    - I saw a really bad one back in 2008
+    """
+    assert expected == actual
